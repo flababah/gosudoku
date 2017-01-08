8d3da8793eb2dd9c3d247c79fc60c0ba4f5a895f
Anders Høst <anders@foob.dk>
2017-01-08 20:43:14 +0100

    Implement priority queue. Run gofmt. Rename file.

--- solver.go
+++ solver.go
@@ -2,25 +2,21 @@ package main
 
 /*
  * Simple GO program to solve sudokus.
+ *
+ * TODO Try to use clz and popcount, link against C.
  */
 
 import (
-	"os"
 	"fmt"
+	"os"
 )
-
-// TODO gør først på færrest sansynlige.
-// TODO sorter contant væk fra working set.
-
-// TODO hvordan er order of type const var etc... ?
-// TODO kør gofmt
 
 type mask uint16 // Enough to hold 9 bits.
 
 type cell struct {
 	groups [3]*mask
-	value uint
-	computed bool
+	next   *cell
+	value  uint
 }
 
 const (
@@ -29,7 +25,7 @@ const (
 
 var (
 	// Group resolvers.
-	resolvers = [3]func(int)int{
+	resolvers = [3]func(int) int{
 
 		// Row.
 		func(i int) int { return i / 9 },
@@ -39,12 +35,12 @@ var (
 
 		// Box.
 		func(i int) int {
-			return (i / 27) * 3 +         // Row
-				(i / 3) - (i / 9) * 3 // Column
+			return (i/27)*3 + // Row
+				(i / 3) - (i/9)*3 // Column
 		},
 	}
-	constraints = [3][9]mask{ }
-	cells = [9*9]cell{ }
+	constraints = [3][9]mask{}
+	cells       = [9 * 9]cell{}
 )
 
 // Link the cell to its appropriate row, column and box constraints.
@@ -74,44 +70,78 @@ func (c *cell) set(candidate uint) {
 	}
 }
 
+func (m mask) popCount() int {
+	count := 0
+
+	for i := uint(0); i != 9; i++ {
+		if int(m)&(1<<i) != 0 {
+			count++
+		}
+	}
+	return count
+}
+
 func exit(format string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, format + "\n", args)
+	fmt.Fprintf(os.Stderr, format+"\n", args)
 	os.Exit(1)
 }
 
 func printSolution() {
 	for i, cell := range cells {
-		if i % 9 == 0 && i != 0 {
+		if i%9 == 0 && i != 0 {
 			fmt.Println()
 		}
-		fmt.Printf("%c ", cell.value + '1')
+		fmt.Printf("%c ", cell.value+'1')
 	}
 }
 
-func solve(index, depth uint) bool {
-	if index == 9 * 9 {
+// Main solving step. Recurses on each cell in the queue. queue is a pointer to
+// the head element in a linked list of cells which don't have a set value.
+func solve(queue **cell) bool {
+	if *queue == nil {
 		return true
 	}
-	cell := &cells[index]
+	bestp := queue
+	best := 9
 
-	if cell.computed {
-		return solve(index + 1, depth + 1)
+	// Find the best candidate to recurse on.
+	for pptr := queue; *pptr != nil; pptr = &(*pptr).next {
+		current := *pptr
+		pop := current.possibilities().popCount()
+		if pop == 1 {
+			bestp = pptr
+			break // Obvious elimination.
+
+		} else if pop < best {
+			bestp = pptr
+			best = pop
+		}
 	}
+	cell := *bestp
+	*bestp = cell.next // Unlink from queue.
+
 	candidates := cell.possibilities()
 	for value := uint(0); value != 9; value++ {
-		if candidates & (1 << value) != 0 {
+		if candidates&(1<<value) != 0 {
 			cell.unset(value)
-			if solve(index + 1, depth + 1) {
-				cell.value = value // Setting here is OK
+			if solve(queue) {
+				// It's OK to set value and not clean up the queue here
+				// since we only reach this point if the puzzle was solved.
+				cell.value = value
 				return true
 			}
 			cell.set(value)
 		}
 	}
+
+	cell.next = *queue // Link back to head of queue.
+	*queue = cell
 	return false
 }
 
 func main() {
+	var head *cell = nil
+
 	// Set default bits in constraints.
 	for i, group := range constraints {
 		for j, _ := range group {
@@ -121,10 +151,10 @@ func main() {
 
 	// Parse input and fill out cells.
 	if len(os.Args) < 2 {
-		exit( "Usage: %s input", os.Args[0])
+		exit("Usage: %s input", os.Args[0])
 	}
 	input := os.Args[1]
-	if len(input) != 9 * 9 {
+	if len(input) != 9*9 {
 		exit("Puzzle is not 9 times 9")
 	}
 	for idx, char := range input {
@@ -135,15 +165,16 @@ func main() {
 		case '1', '2', '3', '4', '5', '6', '7', '8', '9':
 			value := uint(char) - '0' - 1
 			cell.value = value
-			cell.computed = true
 			cell.unset(value)
 			continue
 		case '.':
+			cell.next = head
+			head = cell
 			continue
 		}
 		exit("Invalid charater in input: %c", char)
 	}
-	if !solve(0, 0) {
+	if !solve(&head) {
 		exit("Impossible puzzle")
 	}
 	fmt.Println("Solution found!")
